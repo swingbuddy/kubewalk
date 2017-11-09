08849530588fc0831422741b8ffa814e2c248d43
swingbuddy <33527367+swingbuddy@users.noreply.github.com>
2017-11-09 20:17:48 +0000

    Added initialisation of cluster networking

--- boot.sh
+++ boot.sh
@@ -10,5 +10,13 @@
 # 010 Initializes cluster master node
 kubeadm init --apiserver-advertise-address $(hostname -i)
 
+
+
+# 012 Initialize cluster networking
+kubectl apply -n kube-system -f \
+    "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 |tr -d '\n')"
+    
+    
+    
 # 020 Install a bunch of packages to make a nicer working environment
 yum install -y git less vim which
